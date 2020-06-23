01fa805a1d6d2e9b240622e12f0be6892e707769
Jillo Abdullahi <jillo.abdulla@gmail.com>
2020-06-23 18:29:29 +0300

    add project configuration settings

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,7 @@
 function startGame() {
   // starting game
-  var messagesElement = document.getElementById("message")
+  var messagesElement = document.getElementById("messages");
+  messagesElement!.innerText = "Welcome to MultiMath! Starting new game"
 }
+
+document.getElementById("startGame")!.addEventListener('click', startGame)
